c4307c21462c0d16404a75d9f856d9864591fe30
ValentinWuerz <valentinwuerz@web.de>
2022-01-18 20:19:39 +0100

    working sidemenu

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -4,16 +4,40 @@ import { AfterViewInit, Component, DoCheck, ElementRef, OnInit, ViewChild } from
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss']
 })
-export class SidemenuComponent implements OnInit {
-  @ViewChild('ref1') input: ElementRef<HTMLInputElement>;
-  menu_opened: boolean
+export class SidemenuComponent implements OnInit, AfterViewInit {
+  @ViewChild('menu') menu: ElementRef<HTMLInputElement>;
+  @ViewChild('page') page: ElementRef<HTMLInputElement>;
+  @ViewChild('open') open: ElementRef<HTMLInputElement>;
+  @ViewChild('container') container: ElementRef<HTMLInputElement>;
+  @ViewChild('remaining') remaining: ElementRef<HTMLInputElement>;
+  @ViewChild('img') img: ElementRef<HTMLInputElement>;
+
+  show_menu: boolean = true
 constructor(){}
   ngOnInit(): void {
   }
- 
-  log(){
-    console.log(this.input.nativeElement.style.display='none');
+  ngAfterViewInit(): void {
+    this.calc_box()
 
+  }
+ 
+  open_menu(){
+      this.show_menu=!this.show_menu
+
+  }
+  calc_box(){
+  /*  let img= this.img.nativeElement["naturalHeight"]
+   console.log(img);
+   
+  //this.menu.nativeElement.style.height=img.toString()
+  let box_height= this.page.nativeElement.offsetHeight - 
+  this.container.nativeElement.offsetHeight +
+  this.open.nativeElement.offsetHeight
+  this.remaining.nativeElement.style.height=box_height.toString()
+                 */
+  
+          
+                
   }
   
 }
